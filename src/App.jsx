e7a89d1aefc2be20fd231cb917c83face34bfc9d
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-05-11 15:58:28 -0300

    :construction: começando a home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
 
 import { Outlet } from "react-router-dom";
+import Nav from "./Components/Nav/Index";
+
+const navLinks = [
+  {
+    name: "Lugar 1",
+    path: "/",
+  },
+  {
+    name: "Lugar 2",
+    path: "/",
+  },
+  {
+    name: "Lugar 3",
+    path: "/",
+  },
+];
 
 function App() {
   return (
     <>
+      <Nav navLinks={navLinks} />
       <Outlet />
     </>
   );
