6cb534d2146dea6c1535bdad8b1346c9a94fc238
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-05-13 16:12:45 -0300

    :sparkles: feat: feitas as páginas sobre as cidades francesas

--- src/Data/textDetail.js
+++ src/Data/textDetail.js
@@ -38,11 +38,79 @@ export const details = {
     },
   ],
   strasbourg: [
-    "isso aqui é um teste para strasbourg",
-    "segundo teste para strasbourg",
+    {
+      sectionTitle: "Detalhes Gerais",
+      sectionText: [
+        "Localizada no leste da França, Estrasburgo fica na região administrativa da Alsácia. Além disso, está exatamente na fronteira entre França e Alemanha, a ponto de ter pertencido ao país germânico em alguns momentos da história. ",
+        "A cidade de Estrasburgo e sua região têm diversas atrações e pontos turísticos diferentes para aproveitar. Existem opções para todos os gostos, recheadas de cultura e história. Há ainda todas as atrações natalinas e as delícias da culinária francesa, além de uma interessante influência da cultura alemã, presente na história da cidade e pela proximidade com o país germânico.",
+      ],
+      linkPath: "/",
+      linkName: "Conhecer Mais",
+    },
+    {
+      sectionTitle: "Cultura",
+      sectionText: [
+        "Estrasburgo é uma linda cidade da França que garante uma viagem inesquecível. Possui uma rica história, arquitetura única e pontos turísticos famosos em todo o mundo, além de belos canais e parques cheios de natureza. ",
+        "Além disso, ela é conhecida como a casa de Noel, um lugar em que se respira Natal em qualquer época do ano. Conheça e se apaixone!",
+      ],
+      linkPath: "/",
+      linkName: "Conhecer Mais",
+    },
+    {
+      sectionTitle: "Comidas Típicas",
+      sectionText: [
+        "Os restaurantes em Estrasburgo reúnem o melhor da culinária da Alsácia. Assim como toda cidade francesa, há muito vinho, queijo e pão, mas não se trata apenas disso: alguns dos melhores restaurantes da região estão na cidade, como o La Clocle à Fromage.",
+        "Se você pretende visitar Estrasburgo, prepare-se para uma gastronomia bem diversificada, pois ela possui influências da culinária da França e da Alemanha. Isso acontece porque a cidade já pertenceu ao país vizinhos antigamente; hoje, o reflexo disso não está apenas nas ruas, mas também nos restaurantes.",
+      ],
+      linkPath: "/",
+      linkName: "Conhecer Mais",
+    },
+    {
+      sectionTitle: "Pontos Turísticos",
+      sectionText: [
+        "Visitar os pontos turísticos de Estrasburgo é um verdadeiro mergulho na história da Alsácia, região localizada no nordeste da França que faz fronteira com a Alemanha.",
+        "Justamente graças a essa proximidade com o país vizinho, você perceberá algumas particularidades; uma delas é a mistura de culturas nessa cidade, que já foi alemã em outras décadas.",
+      ],
+      linkPath: "/",
+      linkName: "Conhecer Mais",
+    },
   ],
   bordeaux: [
-    "isso aqui é um teste para bordeaux",
-    "segundo teste para bordeaux",
+    {
+      sectionTitle: "Detalhes Gerais",
+      sectionText: [
+        "Bordeaux, centro de uma famosa região vinícola, é uma cidade portuária às margens do rio Garonne, no sudoeste da França.",
+        "Ela é conhecida pela Catedral Gótica de Santo André, por mansões dos séculos XVIII e XIX e por importantes museus de arte, como o Musée des Beaux-Arts de Bordeaux.",
+      ],
+      linkPath: "/",
+      linkName: "Conhecer Mais",
+    },
+    {
+      sectionTitle: "Cultura",
+      sectionText: [
+        "Selecionado pela Unesco por seu excepcional patrimônio urbano, Bordeaux possui mais de 350 edifícios classificados como monumentos históricos.",
+        "A arte e a cultura encontram-se em todos os cantos das ruas e abrigam-se em alguns belos e importantes museus, incluindo o Muséum de Bordeaux, que reabre suas portas. Siga o guia!",
+      ],
+      linkPath: "/",
+      linkName: "Conhecer Mais",
+    },
+    {
+      sectionTitle: "Comidas Típicas",
+      sectionText: [
+        "É difícil imaginar um lugar com mais dotes culinários que a França, um dos poucos países do mundo que teve sua gastronomia reconhecida como um Patrimônio Cultural da UNESCO. Mas ao contrário do que muitos pensam, o núcleo gastronômico do país não é Paris, a cidade das luzes.",
+        "Uma pesquisa extensa realizada pela revista de culinária Atabula revelou que Bordeaux, que no ano passado foi eleita a melhor cidade do mundo para turistas pela Lonely Planet, é também a melhor culinária da França de acordo com os próprios franceses.",
+      ],
+      linkPath: "/",
+      linkName: "Conhecer Mais",
+    },
+    {
+      sectionTitle: "Pontos Turísticos",
+      sectionText: [
+        "O ponto turístico mais famoso de Bordeaux é a belíssima Place de la Bourse, que ficou ainda mais bonita depois da construção de um espelho d’água que reflete os prédios que a emolduram.",
+        "lém dos museus e monumentos históricos, Bordeaux é famosa pelos restaurantes renomados, sendo uma ótima opção para quem aprecia a gastronomia francesa.",
+      ],
+      linkPath: "/",
+      linkName: "Conhecer Mais",
+    },
   ],
 };
